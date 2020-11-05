489eb573f7e69065306cc633f328e6fbb7ca7b7b
Yaroslav Melnykovych <51370508+Timhpu@users.noreply.github.com>
2020-11-05 15:26:48 +0200

    Add files via upload

---
+++ Lab#2.R
@@ -0,0 +1,34 @@
+#1. Створить вектор v із 100 елементів командою v <- rnorm(100). Для цього
+#вектору виведіть: 10-й елемент; елементи з 10-го по 20-й включно; 10
+#елементів починаючи з 20-го; елементи більше 0.
+#v<-c(1:100)
+v<- rnorm(100)
+v[10]
+v[10:20]# - How to make show in console full size of our element after 9 symblos(округляет до 8)
+v[20:30]# another method...?? v[20:30-1]
+v[(v>0)]
+
+#2. Створити фрейм (data frame) y командою y <- data.frame(a = rnorm(100), b
+#= 1:100, cc = sample(letters, 100, replace = TRUE)). Для цього data frame
+#виведіть: останні 10 строк; строки з 10 по 20 включно; 10-й елемент
+#стовпця b; повністю стовпець cc, при цьому використайте ім’я стовпця.
+     
+       y <- data.frame(a = rnorm(100), 
+                b= 1:100, cc = sample(letters, 100, replace = TRUE))
+       y[91:100,]
+       y[10:20,]
+       y[10,]  #y[10,"a/b/cc"]
+       y["cc"]
+       str(y["cc"])
+       
+#3. Створити вектор z з елементами 1, 2, 3, NA, 4, NA, 5, NA. Для цього
+#вектору: виведіть всі елементи, які не NA; підрахуйте середнє значення
+#всіх елементів цього вектору без NA значень та з NA значеннями.
+       
+       thr<- c(1:3,NA,4,NA,5,NA)
+       mean(thr,na.rm=TRUE)
+       good<- is.na(thr)
+       thr[!good]# ! - logical No
+       sum(thr)# Sum with NA
+       sum(thr[!good])# sum without NA
+       
---
+++ Lab#3.R
@@ -0,0 +1,58 @@
+
+#1. Функція add2(x, y), яка повертає суму двох чисел.
+  add2 <- function(x, y) {
+    x+y
+  }
+  print (add2(8, 4))
+#2. Функція above(x, n), яка приймає вектор та число n, та повертає всі
+#елементі вектору, які більше n. По замовчуванню n = 10.
+  above <- function(x, n=10) {
+    x[x>n]
+  }
+  print (above(c(1, 2, 5, 11, 8, 45, 11, 10)))
+#3. Функція my_ifelse(x, exp, n), яка приймає вектор x, порівнює всі його елементи за допомогою exp з n, та повертає елементи вектору, які відповідають умові expression. Наприклад, my_ifelse(x, “>”, 0) повертає всі
+#елементи x, які більші 0. Exp може дорівнювати “<”, “>”, “<=”, “>=”, “==”. Якщо exp не співпадає ні з одним з цих виразів, повертається вектор x.
+    my_ifelse <- function(x, exp, n) {
+      res <- x
+      if (exp == "<") {
+        res <- x[x < n]
+      }
+      else if (exp == ">") {
+        res <- x[x > n]
+      }
+      else if (exp == "<=") {
+        res <- x[x <= n]
+      }
+      else if (exp == ">=") {
+        res <- x[x >= n]
+      }
+      else if (exp == "==") {
+        res <- x[x == n]
+      }
+      res
+    }
+    print (my_ifelse(c(1, -2, 5, -11, -8, -45, 11, 10), ">", 0))
+    print (my_ifelse(c(3, 4, 5, 2.15, -1, 0.2), "<", 3.14))
+
+
+
+#4. Функція columnmean(x, removeNA), яка розраховує середнє значення (mean) по кожному стовпцю матриці, або data frame. Логічний параметр
+#removeNA вказує, чи видаляти NA значення. По замовчуванню він дорівнює TRUE.
+    columnmean <- function(x, removeNA=TRUE) {
+      res <- matrix(nrow = 1, ncol = length(x[1,]))
+      colnames(res) <- colnames(x)
+      for(i in 1:length(x[1,])){
+        if (removeNA == TRUE) {
+          res[1, i] <- mean(x[,i], na.rm=TRUE)
+        }
+        else{
+          res[1, i] <- mean(x[,i])
+        }
+      }
+      res
+    }
+    
+    m <- matrix(data=cbind(rnorm(10, 0), c(1,1,NA,1,1,1,1,1,1,1), rnorm(10, 5)), nrow=10, ncol=5)
+    df <- data.frame(col1 = rnorm(10, 0),col2 = c(1,1,NA,1,1,1,1,1,1,1), col3=rnorm(10, 5))
+    print(columnmean(m, FALSE))
+    print(columnmean(df))
